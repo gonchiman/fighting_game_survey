61a8dec20bdd044fec8184cdb9d50188512aca3b
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-06-02 13:13:06 +0900

    constants

---
+++ src/constants/columns.py
@@ -0,0 +1,6 @@
+class SteamChartsColumns:
+    MONTH = "Month"
+    AVG_PLAYERS = "Avg. Players"
+    GAIN = "Gain"
+    GAIN_PERCENT = "% Gain"
+    PEAK_PLAYERS = "Peak Players"
---
+++ src/constants/urls.py
@@ -0,0 +1,1 @@
+BASE_URL = "https://steamcharts.com/app/"
