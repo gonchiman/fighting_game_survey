cf146ad26317ebdfb8a57fa9bf55b3c6190ba21f
gonchiman <legendaryyamasama@gmail.com>
2026-06-03 10:15:25 +0900

    upd

--- src/constants/game_titles.py
+++ src/constants/game_titles.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
+
 from src.constants.paths import DATA_DIR
+
 
 @dataclass(frozen=True)
 class GameData:
@@ -11,18 +13,68 @@ class GameData:
 
 
 class GameTitle(Enum):
-    STREET_FIGHTER_6 = GameData(
-        "Street Fighter 6",
-        1364780,
-        DATA_DIR / "street_fighter_6.csv"
-    )
     STREET_FIGHTER_V = GameData(
         "Street Fighter V",
         310950,
         DATA_DIR / "street_fighter_v.csv"
     )
+
+    TEKKEN_7 = GameData(
+        "TEKKEN 7",
+        389730,
+        DATA_DIR / "tekken_7.csv"
+    )
+
+    DRAGON_BALL_FIGHTERZ = GameData(
+        "DRAGON BALL FighterZ",
+        678950,
+        DATA_DIR / "dragon_ball_fighterz.csv"
+    )
+
+    SOULCALIBUR_VI = GameData(
+        "SOULCALIBUR VI",
+        544750,
+        DATA_DIR / "soulcalibur_vi.csv"
+    )
+
+    MORTAL_KOMBAT_11 = GameData(
+        "Mortal Kombat 11",
+        976310,
+        DATA_DIR / "mortal_kombat_11.csv"
+    )
+
+    GUILTY_GEAR_STRIVE = GameData(
+        "GUILTY GEAR -STRIVE-",
+        1384160,
+        DATA_DIR / "guilty_gear_strive.csv"
+    )
+
+    THE_KING_OF_FIGHTERS_XV = GameData(
+        "THE KING OF FIGHTERS XV",
+        1498570,
+        DATA_DIR / "the_king_of_fighters_xv.csv"
+    )
+
+    STREET_FIGHTER_6 = GameData(
+        "Street Fighter 6",
+        1364780,
+        DATA_DIR / "street_fighter_6.csv"
+    )
+
+    MORTAL_KOMBAT_1 = GameData(
+        "Mortal Kombat 1",
+        1971870,
+        DATA_DIR / "mortal_kombat_1.csv"
+    )
+
+    GRANBLUE_FANTASY_VERSUS_RISING = GameData(
+        "Granblue Fantasy Versus: Rising",
+        2157560,
+        DATA_DIR / "granblue_fantasy_versus_rising.csv"
+    )
+
     TEKKEN_8 = GameData(
-        "Tekken 8",
+        "TEKKEN 8",
         1778820,
         DATA_DIR / "tekken_8.csv"
     )
