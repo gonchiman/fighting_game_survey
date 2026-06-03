096fd966ead08d5cfad437141a67a0cb9390cdae
gonchiman <legendaryyamasama@gmail.com>
2026-06-03 13:32:34 +0900

    data/games

--- scripts/init_games_csv.py
+++ scripts/init_games_csv.py
@@ -4,13 +4,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
-from src.constants.paths import DATA_DIR
+from src.constants.paths import DATA_GAMES_DIR
 from src.constants.game_titles import GameTitle
 from src.data.to_csv import ToCSV
 from src.utils.delete_all_files import delete_all_files
 
 
-delete_all_files(DATA_DIR)
+delete_all_files(DATA_GAMES_DIR)
 
 for game_title in GameTitle:
     ToCSV.to_csv(game_title)
--- src/constants/paths.py
+++ src/constants/paths.py
@@ -4,5 +4,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 DATA_DIR = BASE_DIR / "data"
+DATA_GAMES_DIR = DATA_DIR / "games"
 
 PLOT_IMAGES_DIR = BASE_DIR / "plot_images"
